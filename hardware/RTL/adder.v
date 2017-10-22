134951e61e9ddd53dfcebaf014a702e70c0647e7
ludisu13 <ludisu13@gmail.com>
2017-10-22 15:31:57 +0200

    hardware adder

--- hardware/RTL/adder.v
+++ hardware/RTL/adder.v
@@ -1,52 +1,54 @@
 `timescale 1ns / 1ps
 
-module adder #(parameter ADDER_SIZE=4)(
+module adder #(parameter ADDER_SIZE=64)(
     input  wire         clk,
     input  wire         resetn,
     input  wire         start,
     input  wire         subtract,
     input  wire [512:0] in_a,
-    input  wire [512:0] in_b,    
+    input  wire [512:0] in_b,
     output wire [513:0] result,
     output wire         done    
     );
     localparam [2:0]    STATE_IDLE  = 3'b000,//waiting for start
                         STATE_ONE   = 3'b001,//load inputs
-                        STATE_TWO   = 3'b010,//load addition result to output register 
-                        STATE_THREE = 3'b011,//load shifted version to output register 
-                        STATE_FOUR  = 3'b100;//done   
+                        STATE_TWO   = 3'b010,//add 
+                        STATE_THREE = 3'b011;//done 
     reg [511:0] reg_result;
     reg [511:0] operand_a;
     reg [511:0] operand_b;
     wire [ADDER_SIZE:0] add_result;
-
     wire mux_sel;
-    wire mux_sel_result;
-    assign mux_sel=(state==STATE_ONE) ? 1'b1:1'b0;// 1 selects input 0 selects shifted
+    wire [511:0] op_b_sel;
 
-    assign done=(state==STATE_FOUR) ? 1'b1 :1'b0;// 1 selects shifted resuld 0 selects add result
-    assign mux_sel_result=(state==STATE_THREE) ? 1'b1:1'b0;
     wire [1:0]hack;
-    assign hack=in_a[512]+in_b[512]+carry; 
     reg carry;
+    wire [511:0] shifted;
+    wire end_carry;
+    assign op_b_sel=(subtract==1'b1) ? (~in_b):in_b;
+    assign mux_sel=(state==STATE_ONE) ? 1'b1:1'b0;// 1 selects input 0 selects shifted
+    assign done=(state==STATE_THREE) ? 1'b1 :1'b0;// 1 selects shifted resuld 0 selects add result
+    assign end_carry= (subtract==1'b1) ? carry^1:carry;
+    assign hack=in_a[512]+in_b[512]+(end_carry); 
+    assign shifted=reg_result>>ADDER_SIZE;
     always @(posedge clk) begin
         if (resetn==1'b0 )begin
             reg_result <= 0;
-	    operand_a <=0;
+	        operand_a <=0;
             operand_b <=0;
             carry <=0;
         end
         else begin
-            operand_a<= (mux_sel==1'b1)? in_a:((mux_sel_result==1'b1)? operand_a:(operand_a>>ADDER_SIZE)); 		
-            operand_b<= (mux_sel==1'b1)? in_b:((mux_sel_result==1'b1)? operand_b:(operand_b>>ADDER_SIZE)); 
-            reg_result<= (mux_sel_result==1'b0)? ({add_result[ADDER_SIZE-1:0],reg_result[(512-ADDER_SIZE)-1:0]}):(reg_result>>ADDER_SIZE);
-            carry <= (mux_sel_result==1'b0)? add_result[ADDER_SIZE]:carry;
-	    if(state==STATE_FOUR)
+            operand_a<= (mux_sel==1'b1)? in_a:operand_a>>ADDER_SIZE; 		
+            operand_b<= (mux_sel==1'b1)? op_b_sel:operand_b>>ADDER_SIZE; 
+            reg_result <= {add_result[ADDER_SIZE-1:0],shifted[512-1-ADDER_SIZE:0]};
+            carry <= (state==STATE_ONE) ? subtract:add_result[ADDER_SIZE];
+	    if(state==STATE_THREE)
 	    	$display("result %0x ",result);
         end
     end  		
     assign result = {hack[1:0],reg_result};
-    assign add_result = (mux_sel_result==1'b0)?( operand_a[ADDER_SIZE-1:0]+operand_b[ADDER_SIZE-1:0]+carry):add_result;//save power
+    assign add_result = ( operand_a[ADDER_SIZE-1:0]+operand_b[ADDER_SIZE-1:0]+carry);//save power
 
                     
     reg  [7:0] cnt;         //3-bit register
@@ -75,16 +77,12 @@ module adder #(parameter ADDER_SIZE=4)(
                            end
                        STATE_TWO:
                             begin
-                                next_state <= (cnt<((512/ADDER_SIZE)*2-1))?  STATE_THREE:STATE_FOUR;//?latch 
+                                next_state <= (cnt<((512/ADDER_SIZE)))?  STATE_TWO:STATE_THREE;//?latch 
                             end
                        STATE_THREE:
                             begin
-                                next_state <=  STATE_TWO; 
+                                next_state <=  STATE_IDLE; 
                             end
-                       STATE_FOUR: 
-                           begin
-                                next_state <= STATE_IDLE;
-                           end
                        default: 
                            begin
                               next_state <= STATE_IDLE;
--- hardware/tb/tb_adder.v
+++ hardware/tb/tb_adder.v
@@ -103,31 +103,33 @@ module tb_adder();
     wait (done==1);
     result_ok = (result==514'h2);
 
-    $display("result=",result_ok);
+    $display("result ok first add=",result_ok);
     //Test addition with large test vectors. You can generate your own with the magma online calculator
     perform_add(513'h1d7dc5a16218f714565bc7a83ea20e4c0ecb7f8226e9dae7fdaeab517d4449c268af0a34ff2402e74a3a1350d45dc36e1ca7beb4e541105a58c3990be06f30aa9,
                 513'h1f30aa08f81b00a1ee9e7d3af7cc01dba81837e2ea4a9ad9fea14343dbbd95eb2a6c44aef080d3dc83efeb5e474699aa9dbed2f56c4a26796bf7b209b7c9491a8);
     wait (done==1);//this is a TRAP, don't use for debug 
      result_ok = (result==514'h3cae6faa5a33f7b644fa44e3366e1027b6e3b765113475c1fc4fee955901dfad931b4ee3efa4d6c3ce29feaf1ba45d18ba6691aa518b36d3c4bb4b15983879c51);
-    $display("result=",result_ok);
+    $display("result ok second add=",result_ok);
 #1300;/*
 
     /*************TEST SUBTRACTION*************/
     //Check if 1-1=0
-/*    #`CLK_PERIOD;
+$display("SUBTRACTION");
+    #`CLK_PERIOD;
     #`CLK_PERIOD;
     #`CLK_PERIOD;
     perform_sub(513'h1, 513'h1);
     wait (done==1);
     result_ok = (result==514'h0);
+    $display("result ok first subtraction=",result_ok);
 
     //Test subtraction with large test vectors. You can generate your own with the magma online calculator
     perform_sub(513'h17fcaaf8fe3fd0ee41050d74d78c6087b359fc7813e749a65c4981218f36b4da932ce6e09aa0d31ad9fdddd3abd5114a71b7fd733bbca0f2261ddb7d286cf6ee1,
                 513'h1f9a0c360f98cb4e0bdac0d74ee9af59daada1a769b5b2b91a54bb6a5790da2da28b78cc2d14a3f513da8adaef1e148e3a9eaecc4ca7a211bed70da87129fb6c6);
     wait (done==1);
     result_ok = (result==514'h78629ec2eea705a0352a4c9d88a2b12dd8ac5ad0aa3196ed41f4c5b737a5daacf0a16e146d8c2f25c62352f8bcb6fcbc37194ea6ef14fee06746cdd4b742fb81b);
-    $display("result=%x",result);
-  */  
+
+    $display("result ok second subtraction=",result_ok);
     $finish;
 
     end
